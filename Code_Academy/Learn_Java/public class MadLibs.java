bbb687faedaa1e3cffc7386f25a0e45c3b04e8f7
Santiago <59790233+sainzcodes@users.noreply.github.com>
2023-06-20 18:43:46 -0600

    weq

--- Code_Academy/Learn_Java/public class MadLibs.java
+++ Code_Academy/Learn_Java/public class MadLibs.java
@@ -1,6 +1,8 @@
 public class MadLibs {
   /*
-  Your description here
+  This program generates a mad libbed story.
+  Author: Santiago Sainz
+  Date: Jun/19/2023
   */
     public static void main(String[] args){
       
@@ -9,3 +11,5 @@ public class MadLibs {
       String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
     }       
 }
+
+
