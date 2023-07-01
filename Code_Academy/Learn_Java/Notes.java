9c2238dbce8b5dc616403ed5e91ef83b0e537eab
Santiago <59790233+sainzcodes@users.noreply.github.com>
2023-06-30 21:19:39 -0600

    another one

---
+++ Code_Academy/Learn_Java/Notes.java
@@ -0,0 +1,25 @@
+//Addition and Substraction
+
+// public class PlusAndMinus {
+//     public static void main(String[] args) {   
+//         int zebrasInZoo = 8;
+//         int giraffesInZoo = 4;
+
+//         int animalsInZoo = zebrasInZoo + giraffesInZoo;
+//         System.out.println(animalsInZoo);
+//         int numZebrasAfterTrade = zebrasInZoo - 2;
+//         System.out.println(numZebrasAfterTrade);
+//     }
+// }
+
+
+// Multiplication and Division
+public class MultAndDivide {
+    public static void main(String[] args) {   
+        double subtotal = 30;
+    double tax = 0.0875;
+
+    double total = subtotal + tax;
+    System.out.println(total);
+    }
+}
