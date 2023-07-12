8d3a6f22b962e49edbc0cd5a727b892fb2c904ce
Santiago <59790233+sainzcodes@users.noreply.github.com>
2023-07-12 13:03:20 -0600

    yee

--- Code_Academy/Learn_Java/Notes.java
+++ Code_Academy/Learn_Java/Notes.java
@@ -59,9 +59,21 @@
 // } 
 
 // Compound Assigment Operators
+// public class BakeSale {
+//     public static void main(String[] args) {   
+//         int numCookies = 17;
+
+
+//     // Add your code above
+//     System.out.println(numCookies);
+//     }
+// }
+
 public class BakeSale {
     public static void main(String[] args) {   
         int numCookies = 17;
+        numCookies -= 3;
+        numCookies /= 2;
 
 
     // Add your code above
